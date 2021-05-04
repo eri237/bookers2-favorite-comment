bfb1ba8bedc9321e4ef247180ebff9a4e3fb9c79
eri237 <coconatsu3104@gmail.com>
2021-05-04 04:24:23 +0000

    finish

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+ 
+  resources :books, only: [:show, :index, :edit, :create, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :index, :edit, :update]
+  end
